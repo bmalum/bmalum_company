aae9c29e72148b1b45fa8e94b58ec2a73e95d406
Martin Karrer <support@bmalum.com>
2015-11-03 20:17:36 +0100

    Controller Renaming and Home Scrolling

--- web/static/vendor/main.js
+++ web/static/vendor/main.js
@@ -1,15 +1,15 @@
 var bmalumCompany = angular.module('bmalumCompany', ['ngAnimate', 'ui.router', 'duScroll']);
 
-bmalumCompany.controller('MyCtrl', function($scope){
-	var container = angular.element(document.getElementById('top'));
+bmalumCompany.controller('ScrollController', function($scope){
+	var intro = angular.element(document.getElementById('intro'));
     var home = angular.element(document.getElementById('home'));
     var services = angular.element(document.getElementById('services'));
     var consulting = angular.element(document.getElementById('consulting'));
     var about = angular.element(document.getElementById('about'));
     var partners = angular.element(document.getElementById('partners'));
 
-    $scope.toTheTop = function() {
-      container.scrollTop(0, 5000);
+    $scope.intro = function() {
+      container.scrollTo(intro, 750, 1000);
     }
     $scope.services = function() {
       container.scrollTo(services, 750, 1000);
