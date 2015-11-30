6b6f6407b96bb0ce70d0eb9adb402473ab766a4e
Martin Karrer <support@bmalum.com>
2015-11-30 23:24:13 +0100

    Website Update

--- web/static/vendor/main.js
+++ web/static/vendor/main.js
@@ -1,7 +1,7 @@
 var bmalumCompany = angular.module('bmalumCompany', ['ngAnimate', 'ui.router', 'duScroll']);
 
 bmalumCompany.controller('ScrollController', function($scope){
-	var intro = angular.element(document.getElementById('intro'));
+	  var intro = angular.element(document.getElementById('intro'));
     var home = angular.element(document.getElementById('home'));
     var services = angular.element(document.getElementById('services'));
     var consulting = angular.element(document.getElementById('consulting'));
@@ -22,5 +22,49 @@ bmalumCompany.controller('ScrollController', function($scope){
     }
     $scope.partners = function() {
       container.scrollTo(services, 750, 1000);
+    }
+});
+
+
+bmalumCompany.controller('ModalController', function($scope){
+
+    $scope.consulting_modal = function() {
+      console.log('Opening Modal Consulting');
+      angular.element(consulting_modal).modal('show')
+    }
+    $scope.scalability_modal = function() {
+      console.log('Opening Modal Consulting');
+      angular.element(scalability_modal).modal('show')
+    }
+    $scope.storage_modal = function() {
+      console.log('Opening Modal Consulting');
+      angular.element(storage_modal).modal('show')
+    }
+    $scope.assistance_modal = function() {
+      console.log('Opening Modal Consulting');
+      angular.element(assistance_modal).modal('show')
+    }
+    $scope.development_modal = function() {
+      console.log('Opening Modal Consulting');
+      angular.element(development_modal).modal('show')
+    }
+    $scope.hosting_modal = function() {
+      console.log('Opening Modal Consulting');
+      angular.element(hosting_modal).modal('show')
+    }
+});
+
+bmalumCompany.controller('FooterController', function($scope){
+    $scope.agb_modal = function() {
+      console.log('Opening Modal Consulting');
+      angular.element(agb_modal).modal('show')
+    }
+    $scope.impressum_modal = function() {
+      console.log('Opening Modal Consulting');
+      angular.element(impressum_modal).modal('show')
+    }
+    $scope.disclaimer_modal = function() {
+      console.log('Opening Modal Consulting');
+      angular.element(disclaimer_modal).modal('show')
     }
 });
